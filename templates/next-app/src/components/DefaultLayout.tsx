1785f66031f312d66d7a80dd113988a46d31061c
0xBunzy <bunzybuilds@gmail.com>
2024-11-14 18:12:16 -0800

    added modal

--- templates/next-app/src/components/DefaultLayout.tsx
+++ templates/next-app/src/components/DefaultLayout.tsx
@@ -1,10 +1,18 @@
-/** @format */
-
 "use client";
 
 import React, { ReactNode } from "react";
 import { LaserEyesProvider } from "@omnisat/lasereyes";
+import { ThemeProvider as NextThemesProvider } from "next-themes";
 
 export default function DefaultLayout({ children }: { children: ReactNode }) {
-  return <LaserEyesProvider>{children}</LaserEyesProvider>;
+  return (
+    <NextThemesProvider
+      attribute="class"
+      defaultTheme="system"
+      enableSystem
+      disableTransitionOnChange
+    >
+      <LaserEyesProvider>{children}</LaserEyesProvider>
+    </NextThemesProvider>
+  );
 }
